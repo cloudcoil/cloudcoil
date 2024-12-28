afc8490af38a23cba144921c44d20eafc05114f9
Sambhav Kothari <sambhavs.email@gmail.com>
2024-12-28 23:24:03 +0000

    Add async tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -15,3 +15,16 @@ def test_e2e(test_client_set):
         assert corev1.Namespace.get(name).metadata.name == name
         assert output.remove().metadata.name == name
         assert corev1.Namespace.delete(name).status.phase == "Terminating"
+
+
+@pytest.mark.configure_test_cluster(cluster_name="test-cloudcoil", remove=False)
+async def test_async_e2e(test_client_set):
+    with test_client_set:
+        assert (
+            await corev1.Service.async_get("kubernetes", "default")
+        ).metadata.name == "kubernetes"
+        output = await corev1.Namespace(metadata=ObjectMeta(generate_name="test-")).async_create()
+        name = output.metadata.name
+        assert (await corev1.Namespace.async_get(name)).metadata.name == name
+        assert (await output.async_remove()).metadata.name == name
+        assert (await corev1.Namespace.async_delete(name)).status.phase == "Terminating"
