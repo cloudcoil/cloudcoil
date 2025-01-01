e0a0b8215081e20fb4bfb7877b9898c445e7ace9
Sambhav Kothari <sambhavs.email@gmail.com>
2025-01-02 05:14:33 +0530

    Add a mypy plugin and discover registered schemes dynamically

--- cloudcoil/__init__.py
+++ cloudcoil/__init__.py
@@ -1,3 +1,4 @@
+from ._scheme import scheme
 from ._version import __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "scheme"]
--- cloudcoil/_scheme.py
+++ cloudcoil/_scheme.py
@@ -11,25 +11,33 @@ if sys.version_info >= (3, 11):
 else:
     from typing_extensions import Self
 
+import sys
+
 import yaml
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
 
 from cloudcoil.client._resource import GVK, Resource
 
 
-class Scheme:
-    default_packages = ["cloudcoil.kinds"]
-
+class _Scheme:
     def __init__(self):
         self._registry = {}
+        self._registered_modules = set()
 
-    def register(self, kind: Type[Resource]) -> None:
+    def _register(self, kind: Type[Resource]) -> None:
         self._registry[kind.gvk()] = kind
+        self._registry[kind.gvk().model_copy(update={"api_version": ""})] = kind
 
-    def register_all(self, kinds: list[Type[Resource]]) -> None:
+    def _register_all(self, kinds: list[Type[Resource]]) -> None:
         for kind in kinds:
-            self.register(kind)
+            self._register(kind)
 
-    def discover(self, module: str | ModuleType) -> None:
+    def _discover(self, module: str | ModuleType) -> None:
         # Uses pkgutil to discover all modules in module_path recursively
         # and imports them
         # Then it looks for all classes that are subclasses of Resource
@@ -41,7 +49,8 @@ class Scheme:
                 for _, obj in inspect.getmembers(module):
                     # Check if object is a class and subclasses base_class
                     if inspect.isclass(obj) and issubclass(obj, Resource) and obj != Resource:
-                        self.register(obj)
+                        self._registered_modules.add(module_name)
+                        self._register(obj)
             except Exception as e:
                 print(f"Error importing module {module_name}: {e}")
 
@@ -54,7 +63,7 @@ class Scheme:
         for _, module_name, _ in pkgutil.walk_packages(package.__path__, package.__name__ + "."):
             import_and_check_module(module_name)
 
-    def get(self, gvk: GVK) -> Type[Resource]:
+    def _get(self, gvk: GVK) -> Type[Resource]:
         if gvk not in self._registry:
             raise ValueError(f"Resource kind '{gvk}' not registered")
         return self._registry[gvk]
@@ -71,18 +80,22 @@ class Scheme:
         resource = Resource.model_validate(obj)
         if not resource.api_version or not resource.kind:
             raise ValueError("Missing apiVersion or kind")
-        kind = self.get_kind(resource.api_version, resource.kind)
+        kind = self.get(resource.api_version, resource.kind)
         return kind.model_validate(obj)
 
-    def get_kind(self, api_version: str, kind: str) -> Type[Resource]:
+    def get(self, kind: str, api_version: str = "") -> Type[Resource]:
         return self._registry[GVK(api_version=api_version, kind=kind)]
 
     def parse_file(self, path: str | Path) -> list[Resource] | Resource:
         return self.parse(yaml.safe_load(Path(path).read_text()))
 
     @classmethod
-    def get_default(cls: Type[Self]) -> Self:
+    def _get_default(cls: Type[Self]) -> Self:
+        discovered_kinds = entry_points(group="cloudcoil_kinds")
         output = cls()
-        for package in cls.default_packages:
-            output.discover(package)
+        for discovered_kind in discovered_kinds:
+            output._discover(discovered_kind.load())
         return output
+
+
+scheme = _Scheme._get_default()
