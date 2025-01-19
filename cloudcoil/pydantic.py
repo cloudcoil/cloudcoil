af0cba206875f582ce7e0b8fd9a4ec866eb0a7f0
Sambhav Kothari <sambhavs.email@gmail.com>
2025-01-19 20:12:43 +0000

    Validate default values
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>

--- cloudcoil/pydantic.py
+++ cloudcoil/pydantic.py
@@ -4,4 +4,6 @@ import pydantic
 class BaseModel(pydantic.BaseModel):
     model_config = pydantic.ConfigDict(
         populate_by_name=True,
+        validate_default=True,
+        validate_assignment=True,
     )
