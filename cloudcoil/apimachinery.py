9b3b968225b44c7c1e16163eeadea8e5706f5b84
Sambhav Kothari <sambhavs.email@gmail.com>
2025-01-25 21:55:06 +0000

    Add missing models and update readme with the new api (#61)
    This pull request includes several changes to improve the functionality
    and documentation of the CloudCoil project. The most significant changes
    include updates to the `README.md` file to provide more comprehensive
    examples and usage patterns, enhancements to the
    `cloudcoil/codegen/generator.py` file to support new features, and minor
    fixes to the `Makefile` and template files.
    
    Documentation improvements:
    
    *
    [`README.md`](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L14-R14):
    Added detailed examples on building Kubernetes resources using models,
    fluent builder API, and context manager builder API. Updated the link to
    the cookiecutter template.
    [[1]](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L14-R14)
    [[2]](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R75-R314)
    [[3]](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L349-R589)
    
    Code generation enhancements:
    
    *
    [`cloudcoil/codegen/generator.py`](diffhunk://#diff-e20a70c20872264a870680b7b56b68e61f1bb2e5317c1275afc3a94739cbb218L42-R43):
    Introduced a new `delete_value_at_path` function to support deleting
    values at specified JSON paths. Updated `set_value_at_path` to handle
    empty array slots. Modified `process_updates` to handle delete
    operations. Simplified the `rename_classes` function to use regex
    patterns for renaming classes.
    [[1]](diffhunk://#diff-e20a70c20872264a870680b7b56b68e61f1bb2e5317c1275afc3a94739cbb218L42-R43)
    [[2]](diffhunk://#diff-e20a70c20872264a870680b7b56b68e61f1bb2e5317c1275afc3a94739cbb218R165-R323)
    [[3]](diffhunk://#diff-e20a70c20872264a870680b7b56b68e61f1bb2e5317c1275afc3a94739cbb218R383-R385)
    [[4]](diffhunk://#diff-e20a70c20872264a870680b7b56b68e61f1bb2e5317c1275afc3a94739cbb218L345-L347)
    [[5]](diffhunk://#diff-e20a70c20872264a870680b7b56b68e61f1bb2e5317c1275afc3a94739cbb218L773-R869)
    
    Minor fixes:
    
    *
    [`Makefile`](diffhunk://#diff-76ed074a9305c04054cdebb9e9aad2d818052b07091de1f20cad0bbac34ffb52L13-R14):
    Fixed a minor issue in the `fix-lint` target by removing an unnecessary
    period.
    *
    [`cloudcoil/codegen/templates/pydantic_v2/BaseModel.jinja2`](diffhunk://#diff-1ec40bf99182f1b718f59e8cc58139f928ed6abe7ef19337a1617e484eb780f2L14-R14):
    Replaced tabs with spaces in docstrings to ensure consistent
    indentation.
    [[1]](diffhunk://#diff-1ec40bf99182f1b718f59e8cc58139f928ed6abe7ef19337a1617e484eb780f2L14-R14)
    [[2]](diffhunk://#diff-1ec40bf99182f1b718f59e8cc58139f928ed6abe7ef19337a1617e484eb780f2L44-R44)
    
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>

--- cloudcoil/apimachinery.py
+++ cloudcoil/apimachinery.py
@@ -37,7 +37,7 @@ class Quantity(RootModel[str]):
 
         def root(self, value: str, /) -> Self:
             """
-                    Quantity is a fixed-point representation of a number. It provides convenient marshaling/unmarshaling in JSON and YAML, in addition to String() and AsInt64() accessors.
+            Quantity is a fixed-point representation of a number. It provides convenient marshaling/unmarshaling in JSON and YAML, in addition to String() and AsInt64() accessors.
 
             The serialization format is:
 
@@ -80,7 +80,7 @@ class Quantity(RootModel[str]):
 
         def __call__(self, value: str, /) -> Self:
             """
-                    Quantity is a fixed-point representation of a number. It provides convenient marshaling/unmarshaling in JSON and YAML, in addition to String() and AsInt64() accessors.
+            Quantity is a fixed-point representation of a number. It provides convenient marshaling/unmarshaling in JSON and YAML, in addition to String() and AsInt64() accessors.
 
             The serialization format is:
 
@@ -1038,7 +1038,7 @@ class StatusCause(BaseModel):
 
         def field(self, value: Optional[str], /) -> Self:
             """
-                    The field of the resource that has caused this error, as named by its JSON serialization. May include dot and postfix notation for nested attributes. Arrays are zero-indexed.  Fields may appear more than once in an array of causes due to fields having multiple errors. Optional.
+            The field of the resource that has caused this error, as named by its JSON serialization. May include dot and postfix notation for nested attributes. Arrays are zero-indexed.  Fields may appear more than once in an array of causes due to fields having multiple errors. Optional.
 
             Examples:
               "name" - the field "name" on the current resource
@@ -2383,7 +2383,7 @@ class ObjectMeta(BaseModel):
 
         def creation_timestamp(self, value_or_callback=None, /):
             """
-                    CreationTimestamp is a timestamp representing the server time when this object was created. It is not guaranteed to be set in happens-before order across separate operations. Clients may not set this value. It is represented in RFC3339 form and is in UTC.
+            CreationTimestamp is a timestamp representing the server time when this object was created. It is not guaranteed to be set in happens-before order across separate operations. Clients may not set this value. It is represented in RFC3339 form and is in UTC.
 
             Populated by the system. Read-only. Null for lists. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
             """
@@ -2423,7 +2423,7 @@ class ObjectMeta(BaseModel):
 
         def deletion_timestamp(self, value_or_callback=None, /):
             """
-                    DeletionTimestamp is RFC 3339 date and time at which this resource will be deleted. This field is set by the server when a graceful deletion is requested by the user, and is not directly settable by a client. The resource is expected to be deleted (no longer visible from resource lists, and not reachable by name) after the time in this field, once the finalizers list is empty. As long as the finalizers list contains items, deletion is blocked. Once the deletionTimestamp is set, this value may not be unset or be set further into the future, although it may be shortened or the resource may be deleted prior to this time. For example, a user may request that a pod is deleted in 30 seconds. The Kubelet will react by sending a graceful termination signal to the containers in the pod. After that 30 seconds, the Kubelet will send a hard termination signal (SIGKILL) to the container and after cleanup, remove the pod from the API. In the presence of network partitions, this object may still exist after this timestamp, until an administrator or automated process can determine the resource is fully terminated. If not set, graceful deletion of the object has not been requested.
+            DeletionTimestamp is RFC 3339 date and time at which this resource will be deleted. This field is set by the server when a graceful deletion is requested by the user, and is not directly settable by a client. The resource is expected to be deleted (no longer visible from resource lists, and not reachable by name) after the time in this field, once the finalizers list is empty. As long as the finalizers list contains items, deletion is blocked. Once the deletionTimestamp is set, this value may not be unset or be set further into the future, although it may be shortened or the resource may be deleted prior to this time. For example, a user may request that a pod is deleted in 30 seconds. The Kubelet will react by sending a graceful termination signal to the containers in the pod. After that 30 seconds, the Kubelet will send a hard termination signal (SIGKILL) to the container and after cleanup, remove the pod from the API. In the presence of network partitions, this object may still exist after this timestamp, until an administrator or automated process can determine the resource is fully terminated. If not set, graceful deletion of the object has not been requested.
 
             Populated by the system when a graceful deletion is requested. Read-only. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
             """
@@ -2450,7 +2450,7 @@ class ObjectMeta(BaseModel):
 
         def generate_name(self, value: Optional[str], /) -> Self:
             """
-                    GenerateName is an optional prefix, used by the server, to generate a unique name ONLY IF the Name field has not been provided. If this field is used, the name returned to the client will be different than the name passed. This value will also be combined with a unique suffix. The provided value has the same validation rules as the Name field, and may be truncated by the length of the suffix required to make the value unique on the server.
+            GenerateName is an optional prefix, used by the server, to generate a unique name ONLY IF the Name field has not been provided. If this field is used, the name returned to the client will be different than the name passed. This value will also be combined with a unique suffix. The provided value has the same validation rules as the Name field, and may be truncated by the length of the suffix required to make the value unique on the server.
 
             If this field is specified and the generated name exists, the server will return a 409.
 
@@ -2518,7 +2518,7 @@ class ObjectMeta(BaseModel):
 
         def namespace(self, value: Optional[str], /) -> Self:
             """
-                    Namespace defines the space within which each name must be unique. An empty namespace is equivalent to the "default" namespace, but "default" is the canonical representation. Not all objects are required to be scoped to a namespace - the value of this field for those objects will be empty.
+            Namespace defines the space within which each name must be unique. An empty namespace is equivalent to the "default" namespace, but "default" is the canonical representation. Not all objects are required to be scoped to a namespace - the value of this field for those objects will be empty.
 
             Must be a DNS_LABEL. Cannot be updated. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces
             """
@@ -2565,7 +2565,7 @@ class ObjectMeta(BaseModel):
 
         def resource_version(self, value: Optional[str], /) -> Self:
             """
-                    An opaque value that represents the internal version of this object that can be used by clients to determine when objects have changed. May be used for optimistic concurrency, change detection, and the watch operation on a resource or set of resources. Clients must treat these values as opaque and passed unmodified back to the server. They may only be valid for a particular resource or set of resources.
+            An opaque value that represents the internal version of this object that can be used by clients to determine when objects have changed. May be used for optimistic concurrency, change detection, and the watch operation on a resource or set of resources. Clients must treat these values as opaque and passed unmodified back to the server. They may only be valid for a particular resource or set of resources.
 
             Populated by the system. Read-only. Value must be treated as opaque by clients and . More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
             """
@@ -2579,7 +2579,7 @@ class ObjectMeta(BaseModel):
 
         def uid(self, value: Optional[str], /) -> Self:
             """
-                    UID is the unique in time and space value for this object. It is typically generated by the server on successful creation of a resource and is not allowed to change on PUT operations.
+            UID is the unique in time and space value for this object. It is typically generated by the server on successful creation of a resource and is not allowed to change on PUT operations.
 
             Populated by the system. Read-only. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names#uids
             """
@@ -2890,11 +2890,11 @@ class WatchEvent(BaseModel):
 
         def object(self, value_or_callback=None, /):
             """
-                   Object is:
-            * If Type is Added or Modified: the new state of the object.
-            * If Type is Deleted: the state of the object immediately before deletion.
-            * If Type is Error: *Status is recommended; other types may make sense
-              depending on context.
+            Object is:
+             * If Type is Added or Modified: the new state of the object.
+             * If Type is Deleted: the state of the object immediately before deletion.
+             * If Type is Error: *Status is recommended; other types may make sense
+               depending on context.
             """
             if self._in_context and value_or_callback is None:
                 context = RawExtension.BuilderContext()
